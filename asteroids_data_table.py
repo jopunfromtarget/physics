79d7ca3dd06ffdae14401cf00c6ddd627834bba6
jopunfromtarget <landonpungarcher6@gmail.com>
2026-01-18 01:43:45 -0600

    Calculate and print velocity and diameter statistics
    Added calculations for standard deviation and mean of velocity and diameter, and printed the results.

--- asteroids_data_table.py
+++ asteroids_data_table.py
@@ -1,7 +1,19 @@
 from asteroids import clean_data
 import pandas as pd
+import statistics as st
 
 # This file cleanly prints a table of all
 # relevant data.
 df = pd.DataFrame(clean_data)
+
+SD_velocity = st.stdev(df["velocity_km_s"])
+mean_velocity = st.mean(df["velocity_km_s"])
+
+SD_diameter = st.stdev(df["mean_diameter_m"])
+mean_diameter = st.mean(df["mean_diameter_m"])
+
 print(df)
+print(f'Velocity Standard Deviation: {round(SD_velocity, 4)}')
+print(f'Velocity Mean: {round(mean_velocity, 4)}')
+print(f'Diameter Standard Deviation: {round(SD_diameter, 4)}')
+print(f'Diameter Mean: {round(mean_diameter, 4)}')
